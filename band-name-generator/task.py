8a6c0ea2144bf9b2d6baa004f7c62e36d3cc1539
John Grabsky <jgrabsky@hotmail.com>
2025-07-26 11:56:38 -0500

    day 1 band name generator

---
+++ band-name-generator/task.py
@@ -0,0 +1,5 @@
+# This line of code will take an input using the input() function
+print("Welcome to the Band Name Generator")
+city = input("What's the name of this city you grew you up in?\n")
+pet_name = input ("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet_name)
